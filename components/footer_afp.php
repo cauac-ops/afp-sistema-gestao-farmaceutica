d87fad0dd11a4f9d85896cc6aa39dede55cd2d52
cauac-ops <cauacarvalhocaue@gmail.com>
2026-03-21 02:28:27 +0000

    Update footer_afp.php

--- components/footer_afp.php
+++ components/footer_afp.php
@@ -1,5 +1,5 @@
-  </div><!-- end page-body -->
-</div><!-- end main-content -->
+  </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
