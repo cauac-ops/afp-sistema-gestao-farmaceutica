a0543db9a875f5b5d5721d3693af0e1503c9dd46
cauac-ops <cauacarvalhocaue@gmail.com>
2026-03-21 01:41:47 +0000

    Update login_afp.php

--- public/login_afp.php
+++ public/login_afp.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'config.php';
 
 if (isset($_SESSION['id_func'])) {
-    header('Location: dashboard.php');
+    header('Location: pages/dashboard.php');
     exit;
 }
 
